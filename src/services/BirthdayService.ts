1f7454b862d2cdeb4f3029dd987097fee5539c41
jartigag <jartigag@pm.me>
2021-11-06 14:14:24 +0100

    refactor: findBirthdayEmployees

--- src/services/BirthdayService.ts
+++ src/services/BirthdayService.ts
@@ -1,9 +1,7 @@
-import fs from "fs";
-import path from "path";
-import { Employee } from "./../domain/Employee";
 import { OurDate } from "./../domain/OurDate";
 import { SendGreetingsService } from "./SendGreetingsService";
 import { EmailServer } from "../infrastructure/EmailServer";
+import { FindBirthdayEmployeesService } from "./FindBirthdayEmployeesService";
 
 export class BirthdayService {
   greetBirthdayEmployees(
@@ -11,27 +9,15 @@ export class BirthdayService {
     ourDate: OurDate,
     emailServer: EmailServer
   ) {
-    const data = fs.readFileSync(
-      path.resolve(__dirname, `../../resources/${fileName}`),
-      "UTF-8"
+    const employees = FindBirthdayEmployeesService.findBirthdayEmployees(
+      fileName,
+      ourDate
     );
 
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    lines.shift();
-
-    // print all lines
-    lines.forEach((line) => {
-      const employeeData = line.split(", ");
-      const employee = new Employee(
-        employeeData[1],
-        employeeData[0],
-        employeeData[2],
-        employeeData[3]
-      );
+    for (const employee of employees) {
       if (employee.isBirthday(ourDate)) {
         SendGreetingsService.sendGreetings(employee, emailServer);
       }
-    });
+    }
   }
 }
---
+++ src/services/FindBirthdayEmployeesService.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import { Employee } from "./../domain/Employee";
+import { OurDate } from "src/domain/OurDate";
+
+export const FindBirthdayEmployeesService = {
+  findBirthdayEmployees(fileName: string, ourDate: OurDate) {
+    let employees: Array<Employee> = [];
+
+    const data = fs.readFileSync(
+      path.resolve(__dirname, `../../resources/${fileName}`),
+      "UTF-8"
+    );
+
+    // split the contents by new line
+    const lines = data.split(/\r?\n/);
+    lines.shift();
+
+    // print all lines
+    lines.forEach((line) => {
+      const employeeData = line.split(", ");
+      employees.push(
+        new Employee(
+          employeeData[1],
+          employeeData[0],
+          employeeData[2],
+          employeeData[3]
+        )
+      );
+    });
+
+    return employees;
+  },
+};
