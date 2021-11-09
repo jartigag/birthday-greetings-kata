311972001f0254f516fff094d2335f089012a0e4
jartigag <jartigag@pm.me>
2021-11-09 19:54:46 +0100

    refactor: loadEmployeesData

--- src/BirthdayService.ts
+++ src/BirthdayService.ts
@@ -1,55 +1,86 @@
-import fs from 'fs'
-import path from 'path'
-import nodemailer from 'nodemailer'
-import { Employee } from './Employee'
-import { OurDate } from './OurDate'
-import Mail from 'nodemailer/lib/mailer'
-import SMTPTransport from 'nodemailer/lib/smtp-transport'
+import fs from "fs";
+import path from "path";
+import nodemailer from "nodemailer";
+import { Employee } from "./Employee";
+import { OurDate } from "./OurDate";
+import Mail from "nodemailer/lib/mailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 export class BirthdayService {
-    sendGreetings(fileName: string, ourDate: OurDate, smtpHost: string, smtpPort: number) {
-        const data = fs.readFileSync(path.resolve(__dirname, `../resources/${fileName}`), 'UTF-8')
-
-        // split the contents by new line
-        const lines = data.split(/\r?\n/)
-        lines.shift()
-
-        // print all lines
-        lines.forEach((line) => {
-            const employeeData = line.split(', ')
-            const employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
-            if (employee.isBirthday(ourDate)) {
-                const recipient = employee.getEmail()
-                const body = 'Happy Birthday, dear %NAME%!'.replace('%NAME%',
-                    employee.getFirstName())
-                const subject = 'Happy Birthday!'
-                this.sendMessage(smtpHost, smtpPort, 'sender@here.com', subject, body, recipient)
-            }
-        })
+  sendGreetings(
+    fileName: string,
+    ourDate: OurDate,
+    smtpHost: string,
+    smtpPort: number
+  ) {
+    function loadEmployeesData(employeesFileName: string) {
+      const employeesData = fs.readFileSync(
+        path.resolve(__dirname, `../resources/${employeesFileName}`),
+        "UTF-8"
+      );
+      return employeesData;
     }
 
-    async sendMessage(smtpHost: string, smtpPort: number, sender: string,
-        subject: string, body: string, recipient: string) {
+    const data = loadEmployeesData(fileName);
 
-        const message = {
-            host: smtpHost,
-            port: smtpPort,
-            from: sender,
-            to: [recipient],
-            subject,
-            text: body
-        }
+    // split the contents by new line
+    const lines = data.split(/\r?\n/);
+    lines.shift();
 
-        this.deliveryMessage(message)
-    }
+    // print all lines
+    lines.forEach((line) => {
+      const employeeData = line.split(", ");
+      const employee = new Employee(
+        employeeData[1],
+        employeeData[0],
+        employeeData[2],
+        employeeData[3]
+      );
+      if (employee.isBirthday(ourDate)) {
+        const recipient = employee.getEmail();
+        const body = "Happy Birthday, dear %NAME%!".replace(
+          "%NAME%",
+          employee.getFirstName()
+        );
+        const subject = "Happy Birthday!";
+        this.sendMessage(
+          smtpHost,
+          smtpPort,
+          "sender@here.com",
+          subject,
+          body,
+          recipient
+        );
+      }
+    });
+  }
 
-    // made protected for testing :-(
-    protected async deliveryMessage({host, port, ...msg}: Message) {
-        const transport = nodemailer.createTransport({host, port})
+  async sendMessage(
+    smtpHost: string,
+    smtpPort: number,
+    sender: string,
+    subject: string,
+    body: string,
+    recipient: string
+  ) {
+    const message = {
+      host: smtpHost,
+      port: smtpPort,
+      from: sender,
+      to: [recipient],
+      subject,
+      text: body,
+    };
 
-        await transport.sendMail(msg)
-    }
+    this.deliveryMessage(message);
+  }
+
+  // made protected for testing :-(
+  protected async deliveryMessage({ host, port, ...msg }: Message) {
+    const transport = nodemailer.createTransport({ host, port });
+
+    await transport.sendMail(msg);
+  }
 }
 
-export interface Message extends SMTPTransport.Options, Mail.Options {
-}
+export interface Message extends SMTPTransport.Options, Mail.Options {}
