960df4e262240d763bfee7627964bd7592d01e27
jartigag <jartigag@pm.me>
2021-11-10 17:17:30 +0100

    typescript linter

--- src/infrastructure/SMTPGreetingsSender.ts
+++ src/infrastructure/SMTPGreetingsSender.ts
@@ -1,24 +1,24 @@
 import { Employee } from "@domain/Employee";
-//import { GreetingsSender } from "@domain/GreetingsSender";
+// import { GreetingsSender } from "@domain/GreetingsSender";
 import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 export interface Message extends SMTPTransport.Options, Mail.Options {}
 
-//TODO: support this.sendMessage using the GreetingsSender interface
-//export const SMTPGreetingsSender: GreetingsSender = {
+// TODO: support this.sendMessage using the GreetingsSender interface
+// export const SMTPGreetingsSender: GreetingsSender = {
 export const SMTPGreetingsSender = {
-  sendGreetingsToEmployee: function (employee: Employee): void {
+  sendGreetingsToEmployee: (employee: Employee): void => {
     const recipient = employee.getEmail();
     const body = "Happy Birthday, dear %NAME%!".replace(
       "%NAME%",
       employee.getFirstName()
     );
     const subject = "Happy Birthday!";
-    this.sendMessage(
-      "127.0.0.1", //FIXME
-      1025, //FIXME
+    SMTPGreetingsSender.sendMessage(
+      "127.0.0.1", // FIXME
+      1025, // FIXME
       "sender@here.com",
       subject,
       body,
@@ -43,7 +43,7 @@ export const SMTPGreetingsSender = {
       text: body,
     };
 
-    //this.deliveryMessage(message);
+    // this.deliveryMessage(message);
     nodemailer
       .createTransport({ host: "127.0.0.1", port: 1025 })
       //                 FIXME: ^^^^^^^^^ ,       ^^^^
